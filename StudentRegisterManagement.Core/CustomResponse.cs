e132f6580d9df2af27048e6ea8cb40de4baa1108
Hakan Kaban <90520449+HakanKban@users.noreply.github.com>
2023-07-27 18:24:19 +0300

    Custom response yapısı eklendi.

---
+++ StudentRegisterManagement.Core/CustomResponse.cs
@@ -0,0 +1,55 @@
+﻿using StudentRegisterManagement.Core.Dtos;
+using System.Text.Json.Serialization;
+
+namespace StudentRegisterManagement.Core
+{
+    public record CustomResponse<T>
+    {
+        [JsonPropertyName("data")] public T Data { get; set; }
+
+        [JsonIgnore] public int StatusCode { get; set; }
+
+        [JsonIgnore] public bool Succeeded { get; private set; }
+
+        [JsonIgnore] public object[] Args { get; private set; }
+
+        [JsonPropertyName("error")] public ErrorDTO Error { get; set; }
+        public static CustomResponse<T> Success(int statusCode, T data)
+        {
+            return new CustomResponse<T> { Data = data, StatusCode = statusCode, Succeeded = true };
+        }
+
+        public static CustomResponse<T> Success(int statusCode)
+        {
+            return new CustomResponse<T> { StatusCode = statusCode, Succeeded = true };
+        }
+        public static CustomResponse<T> Fail(int statusCode, string error)
+        {
+            return new CustomResponse<T>
+            {
+                StatusCode = statusCode,
+                Error = new ErrorDTO { Details = new List<string> { error } },
+                Succeeded = false
+            };
+        }
+        public static CustomResponse<T> Fail(int statusCode, List<string> errors)
+        {
+            return new CustomResponse<T>
+            {
+                StatusCode = statusCode,
+                Error = new ErrorDTO { Details = errors },
+                Succeeded = false
+            };
+        }
+        public static CustomResponse<T> Fail(int statusCode, List<string> errors, params object[] args)
+        {
+            return new CustomResponse<T>
+            {
+                StatusCode = statusCode,
+                Error = new ErrorDTO { Details = errors },
+                Succeeded = false,
+                Args = args
+            };
+        }
+    }
+}
---
+++ StudentRegisterManagement.Core/Dtos/ErrorDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StudentRegisterManagement.Core.Dtos;
+public class ErrorDTO
+{
+    [JsonPropertyName("errorcode")] public int ErrorCode { get; set; }
+
+    [JsonPropertyName("title")] public string Title { get; set; }
+
+    [JsonPropertyName("details")] public List<string> Details { get; set; }
+
+    [JsonPropertyName("stacktrace")] public string StackTrace { get; set; }
+}
